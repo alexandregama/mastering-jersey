2f5ff73ac0d55fa03a55b15736123bc6b8681ab5
Alexandre Gama <alexandre.gama.lima@gmail.com>
2016-12-15 18:08:06 -0200

    Adds a few fields to Item class

--- src/main/java/com/mastering/jersey/preference/api/Item.java
+++ src/main/java/com/mastering/jersey/preference/api/Item.java
@@ -1,5 +1,7 @@
 package com.mastering.jersey.preference.api;
 
+import java.math.BigDecimal;
+
 import javax.xml.bind.annotation.XmlElement;
 
 public class Item {
@@ -9,6 +11,24 @@ public class Item {
 
 	@XmlElement(name = "title")
 	private String title;
+	
+	@XmlElement(name = "description")
+	private String description;
+	
+	@XmlElement(name = "picture_url")
+	private String pictureUrl;
+	
+	@XmlElement(name = "category_id")
+	private String category;
+	
+	@XmlElement(name = "quantity")
+	private int quantity;
+	
+	@XmlElement(name = "currency_id")
+	private String currency;
+	
+	@XmlElement(name = "unit_price")
+	private BigDecimal price;
 	
 	public Item(Long id, String title) {
 		this.id = id;
@@ -32,6 +52,53 @@ public class Item {
 
 	public void setTitle(String title) {
 		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPictureUrl() {
+		return pictureUrl;
+	}
+
+	public void setPictureUrl(String pictureUrl) {
+		this.pictureUrl = pictureUrl;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", title=" + title + ", description=" + description + ", pictureUrl=" + pictureUrl
+				+ ", category=" + category + ", quantity=" + quantity + ", currency=" + currency + ", price=" + price
+				+ "]";
 	}
 
 }
